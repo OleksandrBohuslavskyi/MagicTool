f4e642048d74d7b33b4b2f7b0c956a7ddbbf162e
Oleksandr Bohuslavskyi <Oleksandr Bohuslavskyi>
2016-07-27 21:13:12 +0300

    [+] Got valid arguments

--- MagicTool/MagicTool/Program.cs
+++ MagicTool/MagicTool/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,39 @@ namespace MagicTool
     {
         static void Main(string[] args)
         {
+            if (args.Length <2)
+            {
+                Console.WriteLine("Invalid arguments!");
+            }
+            else
+            {
+                if (Directory.Exists(args[0]))
+                {
+                    switch (args[1])
+                    {
+                        case "all":
+                            Console.WriteLine(args[1]);
+                            break;
+                        case "срр":
+                            Console.WriteLine(args[1]);
+                            break;
+                        case "reversed1":
+                            Console.WriteLine(args[1]);
+                            break;
+                        case "reversed2":
+                            Console.WriteLine(args[1]);
+                            break;
+                        default:
+                            Console.WriteLine(args[1]);
+                            Console.WriteLine("Invalid command argument!");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid path argument!");
+                }
+            }
         }
     }
 }
